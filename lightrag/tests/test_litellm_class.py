df8a732d74b7fdfe91d361be4f58c5b27822768c
Brandon Phillips <phillipsbrandonit@gmail.com>
2024-08-02 07:13:23 -0600

    added environment tests

--- lightrag/tests/test_litellm_class.py
+++ lightrag/tests/test_litellm_class.py
@@ -1,61 +1,32 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import patch
 from lightrag.core.types import ModelType
 from lightrag.components.model_client import LiteLLMClient
-
+from lightrag.core.generator import Generator
+from lightrag.core.embedder import Embedder
 
 from lightrag.utils import setup_env  # ensure you have .env with OPENAI_API_KEY
 
 # need to setup env
 
 
-class test_lite_model(unittest.TestCase):
-
-    def test_lite_client(self):
-        litellm_client = Mock(spec=LiteLLMClient())
-        print("testing litellm client")
-        kwargs = {
-            # model already defined in the init , add additionnal argument if needed
-            "model": "groq/llama3-70b-8192",
-        }
-        api_kwargs = litellm_client.convert_inputs_to_api_kwargs(
-            input="Hello ?",
-            model_kwargs=kwargs,
-            model_type=ModelType.LLM,
-        ).return_value = {
-            "model": "groq/llama3-70b-8192",
-            "messages": [{"role": "user", "content": "Hello ?"}],
-        }
-
-        assert api_kwargs == {
-            "model": "groq/llama3-70b-8192",
-            "messages": [{"role": "user", "content": "Hello ?"}],
-        }
-
-        output = litellm_client.call(
-            api_kwargs=api_kwargs, model_type=ModelType.LLM
-        ).return_value = {"message": "Hello"}
-
-        assert output == {"message": "Hello"}
-
-    def test_lite_client_embeddings(self):
-        litellm_client = Mock(spec=LiteLLMClient())
-        print("testing litellm client")
-        kwargs = {
-            "model": "text-embedding-ada-002-v2",
-        }
-        api_kwargs = litellm_client.convert_inputs_to_api_kwargs(
-            input="Hello ?",
-            model_kwargs=kwargs,
-            model_type=ModelType.EMBEDDER,
-        ).return_value = {"model": "text-embedding-ada-002-v2", "input": "Helo"}
-
-        assert api_kwargs == {"model": "text-embedding-ada-002-v2", "input": "Helo"}
-
-        output = litellm_client.call(
-            api_kwargs=api_kwargs, model_type=ModelType.EMBEDDER
-        ).return_value = {"message": "Hello"}
-
+class TestEnvironment(unittest.TestCase):
+    
+    def test_no_api_key_raises_error(self):
+        with self.assertRaises(ValueError):
+            client = LiteLLMClient()
+            client.call({"model":"gpt-4o"}, ModelType.LLM)
+    
+    def test_generator_no_api_key_logs_error(self):
+        with self.assertLogs(level="ERROR"):
+            generate = Generator(model_client=LiteLLMClient(), model_kwargs={"model": "gpt-4o"})
+            generate.call({"input_str": "Hello World"})
+    
+    def test_embedder_no_api_key_logs_error(self):
+        # Should this raise a value error or should we send the output back to be handled by the developer?
+        with self.assertRaises(ValueError):
+            embed = Embedder(model_client=LiteLLMClient(), model_kwargs={"model": "text-embedding-ada-002"})
+            embed.call(input="Hello World")
 
 if __name__ == "__main__":
     unittest.main()
