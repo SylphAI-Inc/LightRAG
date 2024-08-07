a8b7ae4018d4c70397054ca164d636eb92d5f608
Brandon Phillips <phillipsbrandonit@gmail.com>
2024-08-07 10:53:23 -0600

    updated tests

--- lightrag/tests/test_litellm_class.py
+++ lightrag/tests/test_litellm_class.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from unittest.mock import patch
 from lightrag.core.types import ModelType
 from lightrag.components.model_client import LiteLLMClient
@@ -11,22 +12,47 @@ from lightrag.utils import setup_env  # ensure you have .env with OPENAI_API_KEY
 
 
 class TestEnvironment(unittest.TestCase):
-    
+
+    @patch.dict(os.environ, clear=True)
     def test_no_api_key_raises_error(self):
         with self.assertRaises(ValueError):
             client = LiteLLMClient()
-            client.call({"model":"gpt-4o"}, ModelType.LLM)
-    
-    def test_generator_no_api_key_logs_error(self):
-        with self.assertLogs(level="ERROR"):
-            generate = Generator(model_client=LiteLLMClient(), model_kwargs={"model": "gpt-4o"})
+            client.call({"model": "gpt-4o"}, ModelType.LLM)
+
+    def test_no_model_logs_error(self):
+        with self.assertRaises(ValueError):
+            generate = Generator(
+                model_client=LiteLLMClient(), model_kwargs={}
+            )
             generate.call({"input_str": "Hello World"})
-    
-    def test_embedder_no_api_key_logs_error(self):
+
+    @patch.dict(os.environ, clear=True)
+    def test_generator_no_api_key_logs_error(self):
+        with self.assertRaises(ValueError):
+            generate = Generator(
+                model_client=LiteLLMClient(), model_kwargs={"model": "gpt-4o"}
+            )
+            generate.call({"input_str": "Hello World"})
+
+    @patch.dict(os.environ, clear=True)
+    def test_embedder_no_api_key_call_logs_error(self):
         # Should this raise a value error or should we send the output back to be handled by the developer?
         with self.assertRaises(ValueError):
-            embed = Embedder(model_client=LiteLLMClient(), model_kwargs={"model": "text-embedding-ada-002"})
+            embed = Embedder(
+                model_client=LiteLLMClient(),
+                model_kwargs={"model": "text-embedding-ada-002"},
+            )
             embed.call(input="Hello World")
+            
+    @patch.dict(os.environ, clear=True)
+    async def test_embedder_no_api_key_acall_logs_error(self):
+        with self.assertRaises(ValueError):
+            embed = Embedder(
+                model_client=LiteLLMClient(),
+                model_kwargs={"model": "text-embedding-ada-002"},
+            )
+            await embed.acall(input="Hello World")
+
 
 if __name__ == "__main__":
     unittest.main()
